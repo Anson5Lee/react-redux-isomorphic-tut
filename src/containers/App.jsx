25f5b6cb5c817160d38c9512f6b0ea1512a06e66
Anson5Lee <18911921113@163.com>
2017-01-06 15:47:25 +0800

    nothing special

---
+++ src/containers/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component, PropTypes } from 'react';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>{this.props.children}</div>
+    )
+  }
+}
+
+App.propTypes = {
+  children: PropTypes.any
+}
---
+++ src/routes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import {
+  App,
+  Home,
+  NotFound
+} from './containers';
+
+export default (store)
